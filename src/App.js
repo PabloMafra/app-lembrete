fc674bfb8b39ec8bfe26fe0be903ade246184d06
Pablo <pablomafra_8@hotmail.com>
2023-03-31 23:29:45 -0300

    início app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Lembrete from './components/Lembrete';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Lembrete/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lembrete.jsx
@@ -0,0 +1,55 @@
+import react from 'react';
+import { useState } from 'react';
+
+function Lembrete (){
+    
+    const [nomeLembrete, setNomeLembrete] = useState("");
+    // useState salva o estado de qualquer tipo de dado
+    const [dataLembrete, setDataLembrete] = useState("");
+    const [listaLembrete, setListaLembrete] = useState([]); //lista de lembrete
+
+
+    function Add (e){
+        e.preventDefault(); // não deixa a tela atualizar
+    
+        /*pegar os dados que estão em nomeLembrete e dataLembrete
+        criar um objeto com eles {nomeLembrete, dataLembrete} "lembrete"*/
+        let lembrete = {
+            nome: nomeLembrete, data: dataLembrete
+        }
+        
+        //salvar na lista de lembrete -> setListaLembrete(objeto)
+        let lista = [];
+
+        console.log(lista);
+        
+
+    }
+
+    function AlterarTexto(e){
+        setNomeLembrete(e.target.value); // altera o vlaor do nomeLembrete com o value que está no input
+    }
+
+    function DataLembrete(e){
+        setDataLembrete(e.target.value);
+    }
+
+    return (
+       <div>
+        <h3>Novo Lembrete</h3>
+        <form>
+            <div>
+                <input type="text" placeholder="Nome do lembrete" onChange={AlterarTexto} value={nomeLembrete}/>
+                
+                <input type="text" placeholder='Data do lembrete (no formato dd/mm/yyyy)' onChange={DataLembrete} value={dataLembrete}/>
+                
+                <div>
+                    <button onClick={Add}>Criar Lembrete</button>
+                </div>
+            </div>
+        </form>
+       </div>
+    )
+}
+
+export default Lembrete;
